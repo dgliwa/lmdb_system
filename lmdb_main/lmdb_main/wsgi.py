82f4c1ab7509a523f8d16f07eab8e3b9cdfffde4
Derek Gliwa <dgliwa@luc.edu>
2014-01-10 13:27:13 -0600

    WSGI

--- lmdb_main/lmdb_main/wsgi.py
+++ lmdb_main/lmdb_main/wsgi.py
@@ -23,8 +23,8 @@ sys.path.insert(0,"/www/lmdb_deploy/www/lmdb_system/lmdb_main")
 sys.path.insert(0,"/www/lmdb_deploy/www/lmdb_system/lmdb_main/lmdb_main")
 
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lmdb_main.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "lmdb_main.settings"
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lmdb_main.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
