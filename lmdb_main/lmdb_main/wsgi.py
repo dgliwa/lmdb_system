198ee047cbb2b48a27a631e35100da1c1e8cf348
Derek Gliwa <dgliwa@luc.edu>
2014-01-10 13:22:25 -0600

    WSGI conf update

--- lmdb_main/lmdb_main/wsgi.py
+++ lmdb_main/lmdb_main/wsgi.py
@@ -19,10 +19,7 @@ import sys
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "lmdb_main.settings"
-sys.path.append('/www/lmdb_deploy/www/lmdb_system/lmdb_main')
-sys.path.append('/www/lmdb_deploy/www/lmdb_system/lmdb_main/lmdb_main')
-sys.path.append('/www/lmdb_deploy/www/lmdb_system')
-
+os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 
 
 
