de1af8f6a57accbd62330ec02edcd78c51387668
Derek Gliwa <dgliwa@luc.edu>
2014-01-29 12:23:15 -0600

    Custom Admin Changes and Secure Maps

--- lmdb_main/customadmin/views.py
+++ lmdb_main/customadmin/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
 from lmdb.decorators import user_uploaded
 from django.http import *
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
 
 
  
@@ -13,5 +14,20 @@ from django.contrib.auth.models import User, Permission
 @user_uploaded
 @login_required(login_url='/lmdb/login/')
 def addUsers(request):
-	perms = Permission.objects.all()
-	return render(request,'customadmin/addUsers.html',{'perms' : perms})
+	groups = Group.objects.all()
+	if request.POST:
+		failed = []
+		size = int(request.POST['size'])
+		for i in range(0,size):
+			form = UserCreationForm()
+			form.initial['username'] = request.POST['username'][i]
+			form.initial['password1'] = request.POST['password1'][i]
+			form.initial['password2'] = request.POST['password2'][i]
+			print form
+			if form.is_valid():
+				form.save()
+				return HttpRequestRedirect('/admin/auth/user/')
+			else:
+				print form.errors
+		return render(request,'customadmin/addUsers.html',{'groups' : groups})
+	return render(request,'customadmin/addUsers.html',{'groups' : groups})
