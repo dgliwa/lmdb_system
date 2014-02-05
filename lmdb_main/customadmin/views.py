37d28248d15c6669436cafcfd9bb306e7f364c01
Derek Gliwa <dgliwa@luc.edu>
2014-02-05 14:27:36 -0600

    Map Zoom, Map base layer addition

--- lmdb_main/customadmin/views.py
+++ lmdb_main/customadmin/views.py
@@ -7,6 +7,7 @@ from lmdb.decorators import user_uploaded
 from django.http import *
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
+from customadmin.forms import *
 import json
 
 
@@ -23,22 +24,27 @@ def addUsers(request):
 		for i in range(0,size):
 			vals = json.loads(json.dumps(request.POST))
 			print vals
-			form = UserCreationForm()
-			if vals['size'] == '1':
+			form = UserCreateForm()
+			x = int(vals['size'])
+			vals.pop('size')
+			if x == 1:
+				form = UserCreateForm(vals)
+				print '1'
 				#form = UserCreationForm(username=request.POST['username'], password1 = request.POST['password1'], password2 = request.POST['password2'])
-			    form.initial['username'] = request.POST['username']
-			    form.initial['password1'] = request.POST['password1']
-			    form.initial['password2'] = request.POST['password2']
+			    # form.initial['username'] = vals['username']
+			    # form.initial['password1'] = vals['password1']
+			    # form.initial['password2'] = vals['password2']
 			else:
-				form.initial['username'] = request.POST['username'][i]
-				form.initial['password1'] = request.POST['password1'][i]
-				form.initial['password2'] = request.POST['password2'][i]
-			print form['password1']
+				for i in range(x):
+				    userform = {'username' : vals['username'][i], 'password1' : vals['password1'][i], 'password2' : vals['password2'][i], 'first_name':vals['first_name'][i], 'last_name' : vals['last_name'][i], 'email' : vals['email'][i]}
+				    form = UserCreateForm(userform)
+				    print 'not 1'
+			#print form.initial['password1']
 			if form.is_valid():
 				form.save()
-				return HttpRequestRedirect('/admin/auth/user/')
+				return HttpResponseRedirect('/admin/auth/user/')
 			else:
-				print
+				print 'fail'
 				#print form.errors
 		return render(request,'customadmin/addUsers.html',{'groups' : groups})
 	return render(request,'customadmin/addUsers.html',{'groups' : groups})
