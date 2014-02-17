89c2ced439c91d40bd92c31f0825fd3c0fa7c15d
Derek Gliwa <dgliwa@luc.edu>
2014-02-17 14:38:37 -0600

    Working Test Chart
    Very basic test chart.  can be found at
    /reporting/testChart/

--- lmdb_main/reporting/urls.py
+++ lmdb_main/reporting/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from reporting import views
 
 urlpatterns = patterns('',
-        url(r'^/$', views.reporting, name='reportingHome'),
-
+        url(r'^$', views.reporting, name='reportingHome'),
+        url(r'^testChart/$', views.reporting_test, name='reportingTest'),
 
      #   url(r'^ineedhelp/', include('apps.help.urls', namespace='otherhelp', app_name='help')),
 
--- lmdb_main/reporting/views.py
+++ lmdb_main/reporting/views.py
@@ -17,6 +17,10 @@ from django.contrib.auth.models import User
 
 from django.contrib.auth.decorators import permission_required
 from lmdb.decorators import user_uploaded 
+import json
+import datetime
+import calendar
+from django.utils.safestring import mark_safe
 
 
 @login_required(login_url='/login/')
@@ -24,3 +28,37 @@ from lmdb.decorators import user_uploaded
 def reporting(request):
     
     return render(request,'reporting/reporting.html',{})
+
+
+def reporting_test(request):
+	date = datetime.datetime.now()
+	year = date.year
+	month = date.month
+	xaxis = []
+	sightings = []
+	collections = []
+	changes = []
+	measurements = []
+	for i in range(12):
+		day = calendar.monthrange(year,month)[1]
+		xaxis.append(calendar.month_name[month] + " " + str(year))
+		a = Sighting.objects.filter(date__range=[str(year)+"-"+str(month)+"-01", str(year)+"-"+str(month)+"-"+str(day)]).count()
+		b = Collection.objects.filter(datecollect__range=[str(year)+"-"+str(month)+"-01", str(year)+"-"+str(month)+"-"+str(day)]).count()
+		c = Change.objects.filter(date__range=[str(year)+"-"+str(month)+"-01", str(year)+"-"+str(month)+"-"+str(day)]).count()
+		d = Measurement.objects.filter(date__range=[str(year)+"-"+str(month)+"-01", str(year)+"-"+str(month)+"-"+str(day)]).count()
+		sightings.append(a)
+		collections.append(b)
+		changes.append(c)
+		measurements.append(d)
+		if month == 1:
+			month = 12
+			year = year - 1
+		else:
+			month = month-1
+	xaxis.reverse()
+	sightings.reverse()
+	collections.reverse()
+	changes.reverse()
+	measurements.reverse()
+	xaxis = mark_safe(xaxis)
+	return render(request, 'reporting/reportingTest.html',{'collections':collections, 'sightings':sightings, 'changes':changes, 'measurements':measurements, 'xaxis' : xaxis})
