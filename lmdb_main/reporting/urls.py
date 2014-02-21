7d83d291672f66d74c127f661cce2f70d9125386
Derek Gliwa <dgliwa@luc.edu>
2014-02-21 14:30:40 -0600

    added new functionality with the cvs reporting. allowed project criteria to be used to filter

--- lmdb_main/reporting/urls.py
+++ lmdb_main/reporting/urls.py
@@ -6,7 +6,8 @@ urlpatterns = patterns('',
         url(r'^$', views.reporting, name='reportingHome'),
         url(r'^testChart/$', views.reporting_test, name='reportingTest'),
         url(r'^general/$', views.generalReporting, name='generalReporting'),
-        url(r'^general/csv/$', views.csvReport, name='csvReport'),
+        url(r'^general/csv/filter/$', views.csvFilterReport, name='csvFilterReport'),
+        url(r'^general/csv/criteria/$', views.csvCriteriaReport, name='csvCriteriaReport'),
 
 
      #   url(r'^ineedhelp/', include('apps.help.urls', namespace='otherhelp', app_name='help')),
--- lmdb_main/reporting/views.py
+++ lmdb_main/reporting/views.py
@@ -75,13 +75,92 @@ def generalReporting(request):
 	measurements = Measurement.objects.all()
 	collections = Collection.objects.all()
 	changes = Change.objects.all()
-	return render(request,'reporting/generalReporting.html',{'sightings':sightings,'measurements':measurements,'collections':collections,'changes':changes})
-
+	projects = Project.objects.all()
+	return render(request,'reporting/generalReporting.html',{'projects':projects,'sightings':sightings,'measurements':measurements,'collections':collections,'changes':changes})
 
 
 @login_required(login_url='/login/')
 @user_uploaded
-def csvReport(request):
+def csvCriteriaReport(request):
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="general_report.csv"'
+	writer = csv.writer(response)
+	#print request
+	if request.POST:
+		dict = request.POST
+		changes = []
+		collections = []
+		measurements = []
+		sightings = []
+		projectIds = []
+		projects = []
+		data = []
+		if 'projectsInput' in dict:
+			projectIds = dict['projectsInput'].split(",")
+			projects = Project.objects.filter(objectid__in=projectIds)
+		if 'dataInput' in dict:
+			data = dict['dataInput'].split(",")
+		else:
+			data = ['Changes', 'Collections', 'Measurements', 'Sightings']
+		if 'Changes' in data:
+			writer.writerow(['Changes'])
+			if len(projects) == 0:
+				changes = Change.objects.all()
+			else:
+				changes = Change.objects.filter(projectid__in=projects)
+			if len(changes)==0:
+				writer.writerow(['','NO CHANGES'])
+			else:
+				writer.writerow(['','Change ID','Project Name','Project Start Date','Project End Date','Project Description', 'Project Location ID', 'Project Owner','Change Description','Change Justification','Parameter Name','Change Date','Person','Change Location ID'])
+				for c in changes:
+					writer.writerow(['',c.objectid, c.projectid.projectname, c.projectid.projectstartdate, c.projectid.projectenddate, c.projectid.projectdescription, c.projectid.locationid, c.projectid.personid.displayname, c.description, c.justification, c.parameterid.commonname, c.date, c.personid.displayname, c.locationid, ])
+					writer.writerow([])
+		if 'Collections' in data:
+			writer.writerow(['Collections'])
+			if len(projects) == 0:
+				collections = Collection.objects.all()
+			else:
+				collections = Collection.objects.filter(projectid__in=projects)
+			if len(collections)==0:
+				writer.writerow(['','NO COLLECTIONS'])
+			else:
+				writer.writerow(['','Collection ID','Project Name','Project Start Date','Project End Date','Project Description', 'Project Location ID', 'Project Owner','Collection Method','Organism Name','Collection Date','Person','Collection Location ID'])				
+				for c in collections:
+					writer.writerow(['',c.objectid, c.projectid.projectname, c.projectid.projectstartdate, c.projectid.projectenddate, c.projectid.projectdescription, c.projectid.locationid, c.projectid.personid.displayname, c.methodcollect, c.organismid.organismname, c.datecollect, c.personid.displayname, c.locationid, ])
+					writer.writerow([])
+		if 'Measurements' in data:
+			writer.writerow(['Measurements'])
+			if len(projects) == 0:
+				measurements = Measurement.objects.all()
+			else:
+				measurements = Measurement.objects.filter(projectid__in=projects)
+			if len(measurements) == 0:
+				writer.writerow(['','NO MEASUREMENTS'])
+			else:
+				writer.writerow(['','Measurement ID','Project Name','Project Start Date','Project End Date','Project Description', 'Project Location ID', 'Project Owner','Measurement Name','Measurement Method','Measurement Quantity','Measurement Units','Measurement Notes','Measurement Medium','Parameter Name','Measurement Date','Person','Measurement Location ID'])
+				for m in measurements:
+					writer.writerow(['',m.objectid, m.projectid.projectname, m.projectid.projectstartdate, m.projectid.projectenddate, m.projectid.projectdescription, m.projectid.locationid, m.projectid.personid.displayname, m.mname, m.mmethod, m.mquant, m.munits, m.notes, m.medium, m.parameterid.commonname, m.date, m.personid.displayname, m.locationid, ])
+					writer.writerow([])
+		if 'Sightings' in data:
+			writer.writerow(['Sightings'])
+			if len(projects) == 0:
+				sightings = Sighting.objects.all()
+			else:
+				sightings = Sighting.objects.filter(projectid__in=projects)
+			if len(sightings)==0:
+				writer.writerow(['','NO MEASUREMENTS'])
+			else:
+				writer.writerow(['','Sighting ID','Project Name','Project Start Date','Project End Date','Project Description', 'Project Location ID', 'Project Owner','Organism Name','Number Sighted','Sighting Date','Sighting Notes','Person','Sighting Location ID'])
+				for s in sightings:
+					writer.writerow(['',s.objectid, s.projectid.projectname, s.projectid.projectstartdate, s.projectid.projectenddate, s.projectid.projectdescription, s.projectid.locationid, s.projectid.personid.displayname, s.organismid.organismname, s.number, s.date, s.notes, s.personid.displayname, s.locationid, ])
+					writer.writerow([])
+
+	return response
+
+
+@login_required(login_url='/login/')
+@user_uploaded
+def csvFilterReport(request):
 	response = HttpResponse(content_type='text/csv')
 	response['Content-Disposition'] = 'attachment; filename="general_report.csv"'
 	writer = csv.writer(response)
@@ -116,7 +195,6 @@ def csvReport(request):
 			writer.writerow(['Collection ID','Project Name','Project Start Date','Project End Date','Project Description', 'Project Location ID', 'Project Owner','Collection Method','Organism Name','Collection Date','Person','Collection Location ID'])
 			for c1 in collections:
 				c = Collection.objects.get(objectid=int(c1))
-				print c.objectid
 				writer.writerow([c.objectid, c.projectid.projectname, c.projectid.projectstartdate, c.projectid.projectenddate, c.projectid.projectdescription, c.projectid.locationid, c.projectid.personid.displayname, c.methodcollect, c.organismid.organismname, c.datecollect, c.personid.displayname, c.locationid, ])
 				writer.writerow([])
 		if len(measurements) != 0:
