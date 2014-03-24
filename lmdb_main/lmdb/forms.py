1b09781b969ee6bc9679d35a1daf8357f753c4c7
Derek Gliwa <derekgliwa@Dereks-MacBook-Pro.local>
2014-03-24 15:56:17 -0500

    added initial dropdown capability to edit organism page

--- lmdb_main/lmdb/forms.py
+++ lmdb_main/lmdb/forms.py
@@ -3,7 +3,7 @@ from models import Parameter, Permit, Event, Project, People, Location, Organism
 from django.forms.extras.widgets import Select
 from django.contrib.auth.models import User
 from validators import *
-from lmdb.models import PeopleProject
+from lmdb.models import *
 
 
 
@@ -85,15 +85,65 @@ class OrganismForm(forms.ModelForm):
 class EditOrganismForm(forms.ModelForm):
     objectid = forms.IntegerField(widget=forms.HiddenInput())
     organismname = forms.CharField()
-    family = forms.CharField() #last added line!!!!  
-    order_field = forms.CharField() 
-    class_field = forms.CharField() # Field renamed because it was a Python reserved word.
-    phylum = forms.CharField()
-    kingdom = forms.CharField()
-    genus = forms.CharField()
+    family = forms.CharField(widget=Select()) #last added line!!!!  
+    order_field = forms.CharField(widget=Select()) 
+    class_field = forms.CharField(widget=Select()) # Field renamed because it was a Python reserved word.
+    phylum = forms.CharField(widget=Select())
+    kingdom = forms.CharField(widget=Select())
+    genus = forms.CharField(widget=Select())
     species = forms.CharField(max_length=255)
+    wetland_designation = forms.CharField(max_length=10, required=False)
+    cvalue = forms.IntegerField(required=False)
+    introduced = forms.IntegerField(widget=forms.RadioSelect(choices=((1,'Yes'),(0,'No'))), required=False)
     class Meta:
         model = Organism
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.pop('instance')
+        #print instance
+        super(EditOrganismForm, self).__init__(*args, **kwargs)
+        self.fields['objectid'] = forms.IntegerField(widget=forms.HiddenInput(),initial=instance.objectid)
+        self.fields['organismname'] = forms.CharField(initial=instance.organismname)
+        self.fields['kingdom']= forms.CharField(initial=instance.kingdom,widget=Select(choices=(('',''),('Bacteria','Bacteria'),('Archaea','Archaea'),('Protista','Protista'),('Plantae','Plantae'),('Fungi','Fungi'),('Animalia','Animalia'))))
+        phylae = Organism.objects.filter(kingdom=instance.kingdom).values('phylum').distinct()
+        choices = []
+        for ph in phylae:
+            choices.append((ph['phylum'],ph['phylum']))
+        tuple(choices)
+        self.fields['phylum'] = forms.CharField(initial=instance.phylum, widget=Select(choices=choices))
+        class_fields = Organism.objects.filter(phylum=instance.phylum).values('class_field').distinct()
+        print class_fields
+        choices = []
+        for cl in class_fields:
+            choices.append((cl['class_field'],cl['class_field']))
+        tuple(choices)
+        self.fields['class_field'] = forms.CharField(initial=instance.class_field, widget=Select(choices=choices))
+        order_fields = Organism.objects.filter(class_field=instance.class_field).values('order_field').distinct()
+        choices = []
+        for order in order_fields:
+            choices.append((order['order_field'],order['order_field']))
+        tuple(choices)
+        self.fields['order_field'] = forms.CharField(initial=instance.order_field, widget=Select(choices=choices))
+        families = Organism.objects.filter(class_field=instance.class_field).values('family').distinct()  
+        choices = []
+        for fam in families:
+            choices.append((fam['family'],fam['family']))
+        tuple(choices)
+        self.fields['family'] = forms.CharField(initial=instance.family, widget=Select(choices=choices))
+        genera = Organism.objects.filter(family=instance.family).values('genus').distinct()
+        choices = []
+        for g in genera:
+            choices.append((g['genus'],g['genus']))
+        tuple(choices)
+        self.fields['genus'] = forms.CharField(initial=instance.genus, widget=Select(choices=choices))
+        self.fields['species'] = forms.CharField(initial=instance.species, max_length=255)
+        self.fields['wetland_designation'] = forms.CharField(initial=instance.wetland_designation, max_length=10, required=False)
+        self.fields['cvalue'] = cvalue = forms.IntegerField(initial=instance.cvalue, required=False)
+        self.fields['introduced'] = forms.IntegerField(initial=instance.introduced, widget=forms.RadioSelect(choices=((1,'Yes'),(0,'No'))), required=False)
+        # peopleproject = PeopleProject.objects.filter(personid=userid)
+        # projects = []
+        # for pp in peopleproject:
+        #     projects.append(pp.projectid.objectid)
+        # self.fields['projectid'].queryset = Project.objects.filter(objectid__in = projects)
 
 class MeasurementForm(forms.ModelForm):
     objectid = forms.IntegerField(widget=forms.HiddenInput())
